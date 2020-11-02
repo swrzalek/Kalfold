0c18424eb721a1ce42f6579b701017a2b084dd2a
swrzalek <stian.uek@gmail.com>
2020-11-02 21:06:58 +0100

    check webpack loaders

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import './style.scss';
+
+const app = document.getElementById('app');
+
+const intitalDiv = document.createElement('div');
+intitalDiv.textContent = 'Kalfold';
+intitalDiv.setAttribute('class', 'initial');
+app.appendChild(intitalDiv);
--- webpack.config.js
+++ webpack.config.js
@@ -2,18 +2,19 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-entry: './src/js/index.js',
+entry: './src/main.js',
 output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'js/bundle.js'
 },
 devServer: {
-    contentBase: './dist'
+    contentBase: './dist',
+    port: 9000
 },
 plugins: [
     new HtmlWebpackPlugin({
        filename: 'index.html',
-       template: './src/index.html' 
+       template: './src/index.html'
     })
 ],
 module: {
@@ -22,13 +23,24 @@ module: {
             enforce: 'pre',
             test: /\.js$/, //using regex to tell babel exactly what files to transcompile
             exclude: /node_modules/, // files to be ignored
-            loader: 'eslint-loader' // specify the loader 
+            loader: 'eslint-loader' // specify the loader
         },
         {
             test: /\.js$/, //using regex to tell babel exactly what files to transcompile
             exclude: /node_modules/, // files to be ignored
-            loader: 'babel-loader' // specify the loader 
-        }
+            loader: 'babel-loader' // specify the loader
+        },
+        {
+            test: /\.s[ac]ss$/i,
+            use: [
+              // Creates `style` nodes from JS strings
+              'style-loader',
+              // Translates CSS into CommonJS
+              'css-loader',
+              // Compiles Sass to CSS
+              'sass-loader',
+            ],
+        },
     ]
 }
 }
